39261f6f0ec86f59fa8ea57588212b1ae28c127b
bipinpatil1994 <bipinpatil1994@gmail.com>
2026-07-07 14:25:22 +0530

    added code  financewebsite folder

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Website Loaded");
+const cards=document.querySelectorAll(".card");
+
+window.addEventListener("scroll",()=>{
+
+cards.forEach(card=>{
+
+const top=card.getBoundingClientRect().top;
+
+if(top<window.innerHeight-100){
+
+card.style.opacity="1";
+
+card.style.transform="translateY(0)";
+
+}
+
+});
+
+});
